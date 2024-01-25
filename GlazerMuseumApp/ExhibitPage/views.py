1c82e4e06ce69d862fd59d63ea80cde9397be2eb
KevinPanasiuk <kevin.panasiuk@spartans.ut.edu>
2024-01-25 10:56:32 -0500

    very basic admin page and very basic exhibit page

--- GlazerMuseumApp/ExhibitPage/views.py
+++ GlazerMuseumApp/ExhibitPage/views.py
@@ -1,7 +1,21 @@
+#exhibit page view
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import ExText
 
 def index(request):
-    output = ExText.titleText
-    return HttpResponse(output)
+    exhibitList = ExText.objects.all()
+    template = loader.get_template("ExhibitPage/index.html")
+    context = {"exhibitList":exhibitList}
+
+    return HttpResponse(template.render(context,request))
+
+def details(request,ExText_id):
+    ExhibitText = ExText.objects.get(id=ExText_id)
+    template = loader.get_template("ExhibitPage/details.html")
+    context = {"ExText":ExhibitText}
+
+    return HttpResponse(template.render(context,request))
+
+
