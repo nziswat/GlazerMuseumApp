bcb3493b0ccc9c4a59fc950dfd86a564e7fc045d
Matthew.Dauria <dauriagoalie31@gmail.com>
2024-01-25 11:15:06 -0500

    Description text to 500 chars

--- GlazerMuseumApp/ExhibitPage/models.py
+++ GlazerMuseumApp/ExhibitPage/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class ExText(models.Model):
     titleText= models.CharField(max_length=200)
-    descText= models.CharField(max_length=200)
+    descText= models.CharField(max_length=500)
     
     def __str__(self):
         return self.titleText
