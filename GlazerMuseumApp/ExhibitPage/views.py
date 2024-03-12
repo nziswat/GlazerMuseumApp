3a290f8da69002d2dba02ee3fd074355b7d06ebf
KevinPanasiuk <kevin.panasiuk@spartans.ut.edu>
2024-03-12 11:08:52 -0400

    Basic voting not quite working

--- GlazerMuseumApp/ExhibitPage/views.py
+++ GlazerMuseumApp/ExhibitPage/views.py
@@ -1,6 +1,7 @@
 #exhibit page view
 from django.http import HttpResponse
 from django.template import loader
+from django.db.models import F
 
 from .models import ExText, PlayTypes
 
@@ -14,9 +15,22 @@ def index(request):
 def details(request,ExText_id):
     exhibitText = ExText.objects.get(id=ExText_id)
     template = loader.get_template("ExhibitPage/details.html")
-    context = {"ExText":exhibitText,"plays":exhibitText.exhibitPlays.play_set.all()}
+    context = {"ExText":exhibitText,"plays":exhibitText.exhibitPlays.play_set.all(),"exid":ExText_id}
    # plays= {"plays":}
     return HttpResponse(template.render(context,request)) #only takes two args, pass all to context
 
-def vote(request, question_id):
-    return HttpResponse(question_id)
+def vote(request, ExText_id):
+    exhibit = ExText.objects.get(id=ExText_id)
+    plays = exhibit.exhibitPlays.play_set.all()
+        
+    choices = plays.get(pk=request.POST["play"])
+
+    for each in choices:
+        each.votes = F("votes") + 1
+        each.save()
+    return HttpResponse(request)
+    
+    
+
+#def vote(request, question_id):
+#    return HttpResponse(question_id)
