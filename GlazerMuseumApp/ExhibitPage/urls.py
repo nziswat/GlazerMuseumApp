d10701a267649439e78334f797b846a33c950ee7
nziswat <kevinpanasiuk@gmail.com>
2024-03-21 13:19:01 -0400

    urls and html fixing

--- GlazerMuseumApp/ExhibitPage/urls.py
+++ GlazerMuseumApp/ExhibitPage/urls.py
@@ -5,6 +5,5 @@ app_name = "ExhibitPage"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:ExText_id>/", views.details, name="details"),
-    path("<int:ExText_id>/vote/", views.vote, name="vote"),
-    path('exhibits/', views.ExhibitPage, name='ExhibitPage')
+    path("<int:ExText_id>/vote/", views.vote, name="vote")
 ]
